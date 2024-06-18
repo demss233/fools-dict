b9a13392f708450185bfbf1ad3f87b95f524639d
Shiv Satyam <84907785+demss233@users.noreply.github.com>
2024-06-19 04:19:38 +0530

    Update word.js

--- models/word.js
+++ models/word.js
@@ -1,12 +1,12 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const Word = new mongoose.Schema({
-	name: {
-		type: String,
-		required: true,
-	},
-})
+    name: {
+        type: String,
+        required: true,
+    },
+});
 
-const wordModel = new mongoose.model('Word', Word);
+const wordModel = new mongoose.model("Word", Word);
 
 module.exports = wordModel;
