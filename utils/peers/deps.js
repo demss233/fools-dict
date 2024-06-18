889af9a12742c2fd87f9aa4b9c5aef39c9df2b51
Shiv Satyam <84907785+demss233@users.noreply.github.com>
2024-06-19 04:17:27 +0530

    Update deps.js

--- utils/peers/deps.js
+++ utils/peers/deps.js
@@ -1,14 +1,17 @@
-const mongoose = require('mongoose')
-const userModel = require('../../models/word');
+const mongoose = require("mongoose");
+const userModel = require("../../models/word");
 
 const presentInDatabase = async (word) => {
-	try {
-		const data = await userModel.findOne({name: word}).then((response) => {
-			return (response !== undefined && response.name === word);
-		}).catch(exception => console.error(exception))
-	} catch(error) {
-		console.error(error);
-	}
-}
+    try {
+        const data = await userModel
+            .findOne({ name: word })
+            .then((response) => {
+                return response !== undefined && response.name === word;
+            })
+            .catch((exception) => console.error(exception));
+    } catch (error) {
+        console.error(error);
+    }
+};
 
 module.exports = presentInDatabase;
