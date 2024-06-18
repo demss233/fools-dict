331d2a118f50cffa94c08454132e8e6092582bb9
Shiv Satyam <84907785+demss233@users.noreply.github.com>
2024-06-19 04:16:51 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,42 +1,57 @@
-const connect = require('./utils/init')
-const path = require('path');
-const fs = require('fs');
-const {pushToDatabase, pushFromDatabase, viewDatabase} = require('./utils/crud');
-const userModel = require('./models/word')
+const connect = require("./utils/init");
+const path = require("path");
+const fs = require("fs");
+const {
+    pushToDatabase,
+    pushFromDatabase,
+    viewDatabase,
+} = require("./utils/crud");
+const userModel = require("./models/word");
 
 const handle = async (callback) => {
-	connect();
+    connect();
 
-	const args = process.argv.slice(2);
-	const word = args[1];
-	if(args[0] === "add") {
-		pushToDatabase(word);
-	} else if(args[0] === "remove") {
-		pushFromDatabase(word);
-	} else if(args[0] === "view") {
-		await viewDatabase();
-	} else {
-		console.log("There are just three commands for you to use\n(i) add\n(ii) remove\n(iii) view\n");
-	}
+    const args = process.argv.slice(2);
+    const word = args[1];
+    if (args[0] === "add") {
+        pushToDatabase(word);
+    } else if (args[0] === "remove") {
+        pushFromDatabase(word);
+    } else if (args[0] === "view") {
+        await viewDatabase();
+    } else {
+        console.log(
+            "There are just three commands for you to use\n(i) add\n(ii) remove\n(iii) view\n",
+        );
+    }
 
-	await callback();
-}
+    await callback();
+};
 
 const update = async () => {
-	await userModel.find({}).then((lazyResponse) => {
-		let buffer = new String();
-		for(let i = 0; i < lazyResponse.length; ++i) {
-			buffer += lazyResponse[i].name;
-			buffer += '\n';
-		}
-		fs.writeFile('db.txt', buffer.length ? buffer : "oops! looks like the database is empty!", (error) => {
-			if(error) {
-				console.error(error);
-			}
-		})
-	}).catch((error) => {
-		console.error(error);
-	})
-}
+    await userModel
+        .find({})
+        .then((lazyResponse) => {
+            let buffer = new String();
+            for (let i = 0; i < lazyResponse.length; ++i) {
+                buffer += lazyResponse[i].name;
+                buffer += "\n";
+            }
+            fs.writeFile(
+                "db.txt",
+                buffer.length
+                    ? buffer
+                    : "oops! looks like the database is empty!",
+                (error) => {
+                    if (error) {
+                        console.error(error);
+                    }
+                },
+            );
+        })
+        .catch((error) => {
+            console.error(error);
+        });
+};
 
 handle(update);
