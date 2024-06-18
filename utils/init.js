b25579bb467c0d86dde977f3671f3d9824e1d7dc
Shiv Satyam <84907785+demss233@users.noreply.github.com>
2024-06-19 04:18:02 +0530

    Update init.js

--- utils/init.js
+++ utils/init.js
@@ -1,21 +1,21 @@
-require('dotenv').config();
-const mongoose = require('mongoose');
-const wordModel = require('../models/word')
+require("dotenv").config();
+const mongoose = require("mongoose");
+const wordModel = require("../models/word");
 
 const connectionParameters = {
-	useUnifiedTopology: true,
-	useNewUrlParser: true,
-}
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+};
 
 const mongoUri = process.env.mongouri;
 
 const connect = async () => {
-	try {
-		await mongoose.connect(mongoUri);
-		// for debugging purposes add a console log here.
-	} catch(error) {
-		console.error(error);
-	}
-}
+    try {
+        await mongoose.connect(mongoUri);
+        // for debugging purposes add a console log here.
+    } catch (error) {
+        console.error(error);
+    }
+};
 
 module.exports = connect;
